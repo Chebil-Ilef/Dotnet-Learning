32aec20b0bfdbd3904ce384f1359f399161241fa
Chebil-Ilef <ilef.chebil@insat.ucar.tn>
2023-08-06 07:52:02 +0000

    first commit

---
+++ Overview/GarbageCollect/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+void DoSomeBigOperation() {
+    // create a large memory allocation that's only used in this function
+    byte[] myArray = new byte[1000000];
+
+    Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+    Console.ReadLine();
+}
+
+// Retrieve and print the total memory allocated
+Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+Console.ReadLine();
+
+// Call the function that allocates a large memory chunk
+DoSomeBigOperation();
+// TODO: After the function completes, force a Garbage Collection 
+GC.Collect();
+
+// Retrieve and print the updated total memory amount
+Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+Console.ReadLine();
